11ecc18abe4e19ce16fe5ee34be89121d22831a4
HelderF2821 <helder.ferreira2821@gmail.com>
2023-05-11 02:42:38 +0200

    travaille chez moi 2

---
+++ app/src/main/java/com/timemanga/Adapter/CategoryAdapter.java
@@ -0,0 +1,28 @@
+package com.timemanga.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.timemanga.R;
+
+public class CategoryAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.view,parent,false);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
