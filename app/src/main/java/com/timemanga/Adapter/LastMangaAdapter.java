4fc85e9c712114bef1210489a99eb8b147741777
HelderF2821 <helder.ferreira2821@gmail.com>
2023-05-15 15:19:16 +0200

    gregor

--- app/src/main/java/com/timemanga/Adapter/LastMangaAdapter.java
+++ app/src/main/java/com/timemanga/Adapter/LastMangaAdapter.java
@@ -7,14 +7,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.timemanga.R;
+
 public class LastMangaAdapter {
 
     public class Viewholder extends RecyclerView.ViewHolder{
-        TextView title;
+        TextView title, time, star;
         ImageView pic;
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+            title=itemView.findViewById(R.id.title);
+            pic=itemView.findViewById(R.id.pi);
         }
     }
 }
