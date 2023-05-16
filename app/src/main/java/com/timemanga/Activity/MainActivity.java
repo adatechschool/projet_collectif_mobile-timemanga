292fa23fe41747e18417b4c04c1c951b20e00646
HelderF2821 <helder.ferreira2821@gmail.com>
2023-05-16 09:28:48 +0200

    Page principal terminer

--- app/src/main/java/com/timemanga/Activity/MainActivity.java
+++ app/src/main/java/com/timemanga/Activity/MainActivity.java
@@ -8,14 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.timemanga.Adapter.CategoryAdapter;
+import com.timemanga.Adapter.LastMangaAdapter;
 import com.timemanga.Domain.CategoryDomain;
+import com.timemanga.Domain.LastMangaDomain;
 import com.timemanga.R;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter,adapter2;
-    private RecyclerView recyclerViewCategory, recyclerViewPopularList;
+    private RecyclerView recyclerViewCategory, recyclerViewlastList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_LTR);
 
         recyclerViewCategory();
+        recyclerViewlastList();
+    }
+
+    private void recyclerViewlastList() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewlastList = findViewById(R.id.view2);
+        recyclerViewlastList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<LastMangaDomain> lastMangaDomains=new ArrayList<>();
+        lastMangaDomains.add(new LastMangaDomain("Cheese Burger", "fast_1", 9.0, 20));
+        lastMangaDomains.add(new LastMangaDomain("Pepperoni Pizza", "fast_2", 8.1, 16));
+        lastMangaDomains.add(new LastMangaDomain("Vegetable Pizza", "fast_3", 9.5, 16));
+
+        adapter2 = new LastMangaAdapter(lastMangaDomains);
+        recyclerViewlastList.setAdapter(adapter2);
     }
 
     private void recyclerViewCategory() {
